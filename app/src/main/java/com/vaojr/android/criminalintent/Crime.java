970a921dfd82fbe60b713ffb3049fdd13e3dfabd
vaogun <vaogun@gmail.com>
2015-05-21 14:59:50 -0500

    Initial commit

---
+++ app/src/main/java/com/vaojr/android/criminalintent/Crime.java
@@ -0,0 +1,28 @@
+package com.vaojr.android.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by him on 5/21/15.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
---
+++ app/src/main/java/com/vaojr/android/criminalintent/CrimeActivity.java
@@ -0,0 +1,17 @@
+package com.vaojr.android.criminalintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class CrimeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+
+}
