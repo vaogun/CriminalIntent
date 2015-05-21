efbd028490a0ad70ecd42c084bc4c12586fd78db
vaogun <vaogun@gmail.com>
2015-05-21 17:50:57 -0500

    Add "wiring widgets in a fragment" on page 142.

--- app/src/main/java/com/vaojr/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/vaojr/android/criminalintent/CrimeFragment.java
@@ -3,6 +3,8 @@ package com.vaojr.android.criminalintent;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,7 @@ import android.widget.TextView;
  */
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
+    private EditText mTitleField;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,7 +28,27 @@ public class CrimeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_crime, container, false);
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // This space intentionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // This space too
+            }
+        });
+
+        return v;
     }
 
 }
