7792683f8eaf4f7aa81ddeed66618455f011e3c1
vaogun <vaogun@gmail.com>
2015-05-21 17:38:32 -0500

    Add "implementing fragment lifecycle methods" on page 139.

---
+++ app/src/main/java/com/vaojr/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,31 @@
+package com.vaojr.android.criminalintent;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_crime, container, false);
+    }
+
+}
