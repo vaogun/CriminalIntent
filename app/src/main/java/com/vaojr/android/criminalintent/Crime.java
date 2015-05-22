d4847e9e100029d69946b583b58bc6da375cdefd
vaogun <vaogun@gmail.com>
2015-05-22 09:51:10 -0500

    Add "upgrading crime" on page 149.

--- app/src/main/java/com/vaojr/android/criminalintent/Crime.java
+++ app/src/main/java/com/vaojr/android/criminalintent/Crime.java
@@ -1,17 +1,36 @@
 package com.vaojr.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
-/**
- * Created by him on 5/21/15.
- */
+
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
 
     public Crime() {
         // Generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 
     public UUID getId() {
