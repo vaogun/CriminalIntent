025840bd228e2bc09a656fed38b6f90ec796cd15
vaogun <vaogun@gmail.com>
2015-05-25 11:48:40 -0500

    Add "responding to list item click" on page 183.

--- app/src/main/java/com/vaojr/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/vaojr/android/criminalintent/CrimeListFragment.java
@@ -2,11 +2,17 @@ package com.vaojr.android.criminalintent;
 
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class CrimeListFragment extends ListFragment {
+
+    private static final String TAG = "CrimeListFragment";
+
     private ArrayList<Crime> mCrimes;
 
     @Override
@@ -20,5 +26,11 @@ public class CrimeListFragment extends ListFragment {
                         android.R.layout.simple_list_item_1,
                         mCrimes);
         setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Crime c = (Crime)(getListAdapter()).getItem(position);
+        Log.d(TAG,c.getTitle() + " was clicked");
     }
 }
