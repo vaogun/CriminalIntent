48cd326d5dee34c4692dea021ff3bcd08d4924ed
vaogun <vaogun@gmail.com>
2015-05-26 20:42:11 -0500

    Add "updating CrimeFragment's view with Crime data" on page 193.

--- app/src/main/java/com/vaojr/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/vaojr/android/criminalintent/CrimeFragment.java
@@ -44,6 +44,7 @@ public class CrimeFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
         mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -66,6 +67,7 @@ public class CrimeFragment extends Fragment {
         mDateButton.setEnabled(false);
 
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
