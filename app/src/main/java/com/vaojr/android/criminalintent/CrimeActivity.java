52776d6b910027e74ba694a98f98c8db9129a48a
vaogun <vaogun@gmail.com>
2015-05-27 14:44:19 -0500

    Add "Fragment arguments" on page 195.

--- app/src/main/java/com/vaojr/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/vaojr/android/criminalintent/CrimeActivity.java
@@ -5,11 +5,16 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 
+import java.util.UUID;
+
 
 public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID)getIntent()
+                .getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+
+        return CrimeFragment.newInstance(crimeId);
     }
 }
