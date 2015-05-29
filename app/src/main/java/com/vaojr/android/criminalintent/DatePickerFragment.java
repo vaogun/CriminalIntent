b9bf3996f46c34ec7b1530d86fa1038f12b3edd9
vaogun <vaogun@gmail.com>
2015-05-29 13:09:30 -0500

    Add "setting a dialog's contents" on page 215.

--- app/src/main/java/com/vaojr/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/vaojr/android/criminalintent/DatePickerFragment.java
@@ -5,12 +5,18 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.view.View;
 
 public class DatePickerFragment extends DialogFragment {
+
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = getActivity().getLayoutInflater()
+                .inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
