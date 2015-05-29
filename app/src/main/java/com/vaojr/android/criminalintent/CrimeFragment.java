c155c8ecdf79639be5bea45b0f651b13bc39822f
vaogun <vaogun@gmail.com>
2015-05-29 12:31:22 -0500

    Add "showing a DialogFragment" on page 214.

--- app/src/main/java/com/vaojr/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/vaojr/android/criminalintent/CrimeFragment.java
@@ -3,6 +3,7 @@ package com.vaojr.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -23,6 +24,8 @@ import java.util.UUID;
 public class CrimeFragment extends Fragment {
     public static final String EXTRA_CRIME_ID =
             "com.vaojr.android.criminalintent.crime_id";
+
+    private static final String DIALOG_DATE = "date";
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -74,7 +77,14 @@ public class CrimeFragment extends Fragment {
 
         mDateButton = (Button)v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment();
+                dialog.show(fm, DIALOG_DATE);
+            }
+        });
 
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
