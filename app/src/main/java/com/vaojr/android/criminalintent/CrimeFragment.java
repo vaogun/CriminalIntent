ee03843f94442d072fc6ac1571340967612b07be
vaogun <vaogun@gmail.com>
2015-05-29 14:53:00 -0500

    Add "passing data to DatePickerFragment" on page 218.

--- app/src/main/java/com/vaojr/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/vaojr/android/criminalintent/CrimeFragment.java
@@ -81,7 +81,8 @@ public class CrimeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 FragmentManager fm = getFragmentManager();
-                DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment
+                        .newInstance(mCrime.getDate());
                 dialog.show(fm, DIALOG_DATE);
             }
         });
--- app/src/main/java/com/vaojr/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/vaojr/android/criminalintent/DatePickerFragment.java
@@ -6,14 +6,55 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.concurrent.Callable;
 
 public class DatePickerFragment extends DialogFragment {
+    public static final String EXTRA_DATE =
+            "com.vaojr.android.criminalintent.date";
+
+    private Date mDate;
+
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_DATE, date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        mDate =(Date)getArguments().getSerializable(EXTRA_DATE);
+
+        // Create a calendar to get the year, month and day
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mDate);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
         View v = getActivity().getLayoutInflater()
                 .inflate(R.layout.dialog_date, null);
+
+        DatePicker datePicker = (DatePicker)v.findViewById(R.id.dialog_date_datePicker);
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                // Translate year, month, day, into a Date object using a calendar
+                mDate = new GregorianCalendar(year, monthOfYear, dayOfMonth).getTime();
+
+                // Update argument to preserve selected value on rotation
+                getArguments().putSerializable(EXTRA_DATE, mDate);
+            }
+        });
 
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
