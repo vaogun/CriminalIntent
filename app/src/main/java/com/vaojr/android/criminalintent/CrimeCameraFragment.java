de193ebf46bd125df720bf23c4af6243e726b22a
vaogun <vaogun@gmail.com>
2015-06-07 12:26:22 -0500

    Add "using the camera API" on page 304

--- app/src/main/java/com/vaojr/android/criminalintent/CrimeCameraFragment.java
+++ app/src/main/java/com/vaojr/android/criminalintent/CrimeCameraFragment.java
@@ -1,14 +1,20 @@
 package com.vaojr.android.criminalintent;
 
 import android.hardware.Camera;
+import android.hardware.Camera.Size;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import java.io.IOException;
+import java.util.List;
 
 public class CrimeCameraFragment extends Fragment {
     private static final String TAG = "CrimeCameraFragment";
@@ -31,8 +37,86 @@ public class CrimeCameraFragment extends Fragment {
         });
 
         mSurfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceView);
+        SurfaceHolder holder = mSurfaceView.getHolder();
+        // setType() and SURFACE_TYPE_PUSH_BUFFERS are both depreciated,
+        // but are required for Camera preview to work on pre-3.0 devices.
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                // Tell the camera to use this surface as its preview area
+                try {
+                    if (mCamera != null) {
+                        mCamera.setPreviewDisplay(holder);
+                    }
+                } catch (IOException exception) {
+                    Log.e(TAG, "Error setting up preview display", exception);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                if (mCamera == null) return;
+
+                // The surface has changed size; update the camera preview size
+                Camera.Parameters parameters = mCamera.getParameters();
+                Size s = getBestSupportedSize(parameters.getSupportedPreviewSizes(), width, height);
+                parameters.setPreviewSize(s.width, s.height);
+                mCamera.setParameters(parameters);
+                try {
+                    mCamera.startPreview();
+                } catch (Exception e) {
+                    Log.e(TAG, "Could not start preview", e);
+                    mCamera.release();
+                    mCamera = null;
+                }
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                // We can no longer display on this surface, so stop the preview
+                if (mCamera != null) {
+                    mCamera.stopPreview();
+                }
+            }
+        });
 
         return v;
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mCamera = Camera.open(0);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    /** A simple algorithm to get the largest size available. For a more
+     * robust version, see CameraPreview.java in the ApiDemo
+     * sample app from Android.
+     */
+    private Size getBestSupportedSize(List<Size> sizes, int width, int height) {
+        Size bestSize = sizes.get(0);
+        int largestArea = bestSize.width * bestSize.height;
+        for (Size s : sizes) {
+            int area = s.width * s.height;
+            if (area > largestArea) {
+                bestSize = s;
+                largestArea = area;
+            }
+        }
+        return bestSize;
     }
 }
 
--- app/src/main/java/com/vaojr/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/vaojr/android/criminalintent/CrimeFragment.java
@@ -3,6 +3,7 @@ package com.vaojr.android.criminalintent;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -18,6 +19,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import java.util.Date;
 import java.util.UUID;
@@ -37,6 +39,7 @@ public class CrimeFragment extends Fragment {
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
+    private ImageButton mPhotoButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -113,6 +116,23 @@ public class CrimeFragment extends Fragment {
                 mCrime.setSolved(isChecked);
             }
         });
+
+        mPhotoButton = (ImageButton)v.findViewById(R.id.crime_imageButton);
+        mPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), CrimeCameraActivity.class);
+                startActivity(i);
+            }
+        });
+
+        // If camera is not available, disable camera functionality
+        PackageManager pm = getActivity().getPackageManager();
+        boolean hasACamera = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)
+                || pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT);
+        if (!hasACamera) {
+            mPhotoButton.setEnabled(false);
+        }
 
         return v;
     }
